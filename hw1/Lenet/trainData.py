033ff63131a7d3ae8c3a043c9e25261915f8ffa6
CHAO-YU HUANG <b608390@gmail.com>
2019-10-16 12:25:47 +0800

    Update trainData.py

--- hw1/Lenet/trainData.py
+++ hw1/Lenet/trainData.py
@@ -31,6 +31,7 @@ transform_img = transforms.Compose([
 train_data = torchvision.datasets.ImageFolder(root=TRAIN_DATA_PATH, transform=transform_img)
 train_data_loader = data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
+
 # default network
 class Net(torch.nn.Module):
     def __init__(self):
@@ -50,29 +51,33 @@ class Net(torch.nn.Module):
         out = self.relu(self.fc1(out))
         out = self.relu(self.fc2(out))
         out = self.fc3(out)
+        
         return out
 
+    
 net = Net()
 net.cuda()
-optimizer = torch.optim.Adam(net.parameters(), lr=LR)   # optimize all cnn parameters
-loss_func = nn.CrossEntropyLoss()                       # the target label is not one-hotted
+optimizer = torch.optim.Adam(net.parameters(), lr=LR) # optimize all cnn parameters
+loss_func = nn.CrossEntropyLoss()                     # the target label is not one-hotted
+
 
 def trainData():
     for epoch in range(EPOCH):
-        for step, (b_x, b_y) in enumerate(train_data_loader):   # gives batch data, normalize x when iterate train_loader
+        for step, (b_x, b_y) in enumerate(train_data_loader): # gives batch data, normalize x when iterate train_loader
             b_x = b_x.cuda()
             b_y = b_y.cuda()
-            output = net(b_x)            # cnn output
-            loss = loss_func(output, b_y)   # cross entropy loss
-            optimizer.zero_grad()           # clear gradients for this training step
-            loss.backward()                 # backpropagation, compute gradients
-            optimizer.step()                # apply gradients
+            output = net(b_x) # cnn output
+            loss = loss_func(output, b_y) # cross entropy loss
+            optimizer.zero_grad() # clear gradients for this training step
+            loss.backward() # backpropagation, compute gradients
+            optimizer.step() # apply gradients
             
             if step % 50 == 0:
                 loss.data = loss.data.cpu()
                 print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy())
     torch.save(net, 'D:/user/Desktop/lenet.pkl')
-    
+ 
+
 if __name__ == '__main__':
     trainData()
     
